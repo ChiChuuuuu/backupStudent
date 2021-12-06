7fc8bc3ad0c9f82d182c1e9c144578371a7f5942
ChiChuuuuuu <buda.phequa123@gmail.com>
2021-12-06 12:26:37 +0700

    do an 6/12

--- resources/views/grade/show.blade.php
+++ resources/views/grade/show.blade.php
@@ -27,23 +27,27 @@
                             <tr>
                                 <td>{{ $grade->nameSub }}</td>
                                 <td>
-                                    <div @if ($grade->Skill1 <= 5) style="color:red;" @endif>
+                                    <div @if ($grade->Skill1 < 5) style="color:red;" @endif>
                                         {{ $grade->Skill1 }}
                                     </div>
                                 </td>
                                 <td>
-                                    <div @if ($grade->Final1 <= 5) style="color:red;" @endif>
+                                    <div @if ($grade->Final1 < 5) style="color:red;" @endif>
                                         {{ $grade->Final1 }}
                                     </div>
                                 </td>
                                 <td>
-                                    @if( $grade->Skill1 <= 5 || $grade->Final1 <= 5)
+                                    @if( $grade->Skill1 < 5 || $grade->Final1 < 5)
                                         Thi lại
                                     @else
                                         Qua môn
                                     @endif
                                 </td>
+                                @if ($grade->Skill2 != NULL && $grade->Final2 != NULL)
                                 <td><a href="{{ route('grade2.show', $grade->idStudent) }}">Xem điểm thi lại</a></td>
+                                @else
+                                <td></td>
+                                @endif
                             </tr>
                         @endforeach
                     </tbody>
